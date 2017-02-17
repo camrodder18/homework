fcf66beed96ae2e91b24e14fb4f9f6f7d85f1f13
jonathan young <jonathan young>
2017-02-16 21:22:15 -0600

    Initial add

---
+++ homework1.py
@@ -0,0 +1,13 @@
+# Get Speed and Hours
+speed = float(input('Enter the speed in mph: '))
+
+time = float(input('How many hours traveled? '))
+distanceTraveled = float(0.0)
+
+print ('Hours \t Distance Traveled')
+print ('----------------------------')
+
+# Calculate Distance Traveled
+for time in range (1, 1 + int(time)):
+    distanceTraveled = speed * time
+    print (time, '\t', distanceTraveled, 'miles')
